6cf4f01d27b519487064eb73c4bdb580ebcffcac
k-mabbott <kmabbott9@gmail.com>
2023-10-02 16:08:31 -0700

    daily algo

--- Slice-Concat.js
+++ Slice-Concat.js
@@ -90,13 +90,13 @@ console.log(result6, "should be", expected6);
   Output: one NEW array with the items of both in the original order
 */
 
-const arrA1 = ["a", "b"];
-const arrB1 = [1, 2, 3];
-const expected1 = ["a", "b", 1, 2, 3];
+// const arrA1 = ["a", "b"];
+// const arrB1 = [1, 2, 3];
+// const expected1 = ["a", "b", 1, 2, 3];
 
-const arrA2 = [1, 2, 3];
-const arrB2 = ["a", "b"];
-const expected2 = [1, 2, 3, "a", "b"];
+// const arrA2 = [1, 2, 3];
+// const arrB2 = ["a", "b"];
+// const expected2 = [1, 2, 3, "a", "b"];
 
 /**
  * Concatenates the given arrays together into order that they are passed in.
@@ -114,8 +114,8 @@ function concat(items1, items2) {
 }
 
 // Tests
-const result1 = concat(arrA1, arrB1);
-console.log(result1, "should be", expected1);
+// const result1 = concat(arrA1, arrB1);
+// console.log(result1, "should be", expected1);
 
-const result2 = concat(arrA2, arrB2);
-console.log(result2, "should be", expected2);
+// const result2 = concat(arrA2, arrB2);
+// console.log(result2, "should be", expected2);
