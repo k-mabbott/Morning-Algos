226e84bb7edc4a20d11cfb2e2607fa65d4040e0f
k-mabbott <kmabbott9@gmail.com>
2023-10-12 15:43:20 -0700

    commit

--- mern_stack_algos/selectionSort.js
+++ mern_stack_algos/selectionSort.js
@@ -41,16 +41,16 @@ function selectionSort(numbers = []) {
     if (numbers.length < 2) return numbers;
     let min;
     let mindex;
-    for (let i = 0; i < numbers.length-1; i++) {
+    for (let i = 0; i < numbers.length - 1; i++) {
         mindex = i;
         min = numbers[mindex];
         for (let j = i; j < numbers.length; j++) {
-            if (numbers[j]<min){
+            if (numbers[j] < min) {
                 min = numbers[j];
                 mindex = j;
             }
-        } 
-        [numbers[mindex],numbers[i]] = [numbers[i],numbers[mindex]]
+        }
+        [numbers[mindex], numbers[i]] = [numbers[i], numbers[mindex]]
         // let temp = numbers[i];
         // numbers[i] = numbers[mindex];
         // numbers[mindex] = temp;
@@ -60,17 +60,41 @@ function selectionSort(numbers = []) {
 
 
 
-console.log(selectionSort(numbersRandomOrder))
-console.log(selectionSort(numbersReversed))
-console.log(selectionSort([0,1]))
+// console.log(selectionSort(numbersRandomOrder))
+// console.log(selectionSort(numbersReversed))
+// console.log(selectionSort([0, 1]))
 
 
 /*****************************************************************************/
 
 
+let recipe = {flour: 500, sugar: 200, eggs: 1};
+let available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
 
+let recipe2 = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+let available2 = {sugar: 500, flour: 2000, milk: 2000};
 
+console.log(cakes(recipe, available))
+console.log(cakes(recipe2, available2))
 
+function cakes(recipe, available) {
+    // let i = 0;
+    let amount = Infinity;
+    let temp;
+    for (let key in recipe) {
+        // console.log(key, recipe[key]);
+        if (available.hasOwnProperty(key)){
+            console.log(Math.floor(available[key] / recipe[key]) );
+            temp = Math.floor(available[key] / recipe[key])
+            if (temp < amount) {
+                amount = temp;
+            }
+        } else {
+            return 0;
+        }
+    }
+    return amount;
+}
 
 
 
