a28d6f181ee3be16c4bbe65f04f8721ea10d5a61
k-mabbott <kmabbott9@gmail.com>
2023-11-21 10:54:46 -0800

    fixes

--- mern_stack_algos/insertionSort.js
+++ mern_stack_algos/insertionSort.js
@@ -1,39 +1,39 @@
 /* 
-  - Visualization with playing cards (scroll down):
-      https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
-  - Array / bar visualization:
-      https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
+- Visualization with playing cards (scroll down):
+    https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
+- Array / bar visualization:
+    https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
 
-  - Stable sort, efficient for small data sets or mostly sorted data sets.
+- Stable sort, efficient for small data sets or mostly sorted data sets.
 
-  Time Complexity
+Time Complexity
     - Best: O(n) linear when array is already sorted.
     - Average: O(n^2) quadratic.
     - Worst: O(n^2) quadratic when the array is reverse sorted.
 
-  Space: O(1) constant.
+Space: O(1) constant.
 
-  - this sort splits the array into two portions (conceptually, not literally).
-  - the left portion will become sorted, the right portion
-      (that hasn't been iterated over yet) is unsorted.
+- this sort splits the array into two portions (conceptually, not literally).
+- the left portion will become sorted, the right portion
+    (that hasn't been iterated over yet) is unsorted.
 
-  can shift OR swap target element until it reaches desired position
-  shifting steps:
-  1. consider the first item as sorted
-  2. move to the next item
-  3. store current item in a temp var (to make this position available to shift items)
-  4. if item to the left of current is greater than current item, shift the
-      left item to the right.
-  5. repeat step 4 as many times as needed
-  6. insert current item
-  7. move to the next item and repeat
+can shift OR swap target element until it reaches desired position
+shifting steps:
+1. consider the first item as sorted
+2. move to the next item
+3. store current item in a temp var (to make this position available to shift items)
+4. if item to the left of current is greater than current item, shift the
+    left item to the right.
+5. repeat step 4 as many times as needed
+6. insert current item
+7. move to the next item and repeat
 
-  swap steps:
-  1. consider the first item as sorted
-  2. move to the next item
-  4. if item to left of current item is less than current, swap
-  5. repeat step 4 until item to left is less than current item
-  6. move to next item and repeat
+swap steps:
+1. consider the first item as sorted
+2. move to the next item
+4. if item to left of current item is less than current, swap
+5. repeat step 4 until item to left is less than current item
+6. move to next item and repeat
 */
 
 const numbersOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -53,27 +53,30 @@ function insertionSort(numbers = []) {
     if (numbers.length < 2) return numbers;
 
     for (let i = 1; i < numbers.length; i++) {
-        let j = i-1;
-        while (numbers[i] < numbers[j] && j > -1 ){
-            let temp = numbers[i];
-            numbers[i] = numbers[j];
+        let curr = numbers[i];
+        let j = i - 1;
+        while (curr < numbers[j] && j > -1) {
+            let temp = numbers[j+1];
+            numbers[j+1] = numbers[j];
             numbers[j] = temp;
             j--;
         }
-        
+        numbers[j+1] = curr;
     }
 
 
     return numbers
 }
 
+
+
 //[9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
 
-console.log(insertionSort([9,3,6,1]))
+console.log(insertionSort([9, 3, 6, 1]))
 console.log(insertionSort(numbersRandomOrder))
 //[ 3, 6, 9, 1 ]
 
 
-let url = "https://pokeapi.co/api/v2/pokemon-species/125/"
-let res = url.split("/")
-console.log(res[res.length-2])
+// let url = "https://pokeapi.co/api/v2/pokemon-species/125/"
+// let res = url.split("/")
+// console.log(res[res.length - 2])
